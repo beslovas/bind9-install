c2cc31d22d1427a2e3ef70b72d47f82227d7c3e1
Tadas Beslovas <tadas.beslovas@gmail.com>
2022-06-08 15:17:30 +0300

    typo

--- bind9-install.sh
+++ bind9-install.sh
@@ -93,7 +93,7 @@ setup_bind9()
             ;;
     esac
 
-    [[ ! -d "$ZONES_PATH/zones"]] && mkdir "$ZONES_PATH/zones"
+    [[ ! -d "$ZONES_PATH/zones" ]] && mkdir "$ZONES_PATH/zones"
 
     cat << EOF > /etc/bind/named.conf.options
 options {
